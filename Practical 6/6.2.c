a7b1109cb3602eda0fae99702c315218ed5558d3
aishwaryasharma10122 <aishwaryasharma2711@gmail.com>
2025-12-26 12:28:25 +0530

    Update 6.2.c

--- Practical 6/6.2.c
+++ Practical 6/6.2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {            // PRACTICAL 06: Q no.2 :-Sum of even numbers using continue//
-    int n, sum = 0;
+    int n, sum = 0;    //Aishwarya Sharma, ERP-10122//
 
     printf("Enter a positive number: ");
     scanf("%d", &n);
@@ -16,4 +16,7 @@ int main() {            // PRACTICAL 06: Q no.2 :-Sum of even numbers using cont
     printf("Sum of even numbers from 1 to %d = %d\n", n, sum);
 
     return 0;
-}
+} 
+//Output//
+//Enter a positive number: 4//
+//Sum of even numbers from 1 to 4 = 6//
