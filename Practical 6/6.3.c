e6c592872fd7a7fddb02b1208b7d9428b5de0f81
aishwaryasharma10122 <aishwaryasharma2711@gmail.com>
2025-12-26 18:51:01 +0530

    Update 6.3.c

--- Practical 6/6.3.c
+++ Practical 6/6.3.c
@@ -23,14 +23,14 @@ int main() {                 //PRACTICAL 06: Q.no.3:- ATM Pin Authentication//
     return 0;
 } 
 //Output//
-//Enter your 4-digit ATM PIN: 2711//
-//Incorrect PIN. Attempts left: 2//
-//Enter your 4-digit ATM PIN: 7896//
-//Incorrect PIN. Attempts left: 1//
-//Enter your 4-digit ATM PIN: 7601//
-//Incorrect PIN. Attempts left: 0//
-//Maximum attempts reached. ATM card blocked.//
+//Enter your 4-digit ATM PIN: 2711
+//Incorrect PIN. Attempts left: 2
+//Enter your 4-digit ATM PIN: 7896
+//Incorrect PIN. Attempts left: 1
+//Enter your 4-digit ATM PIN: 7601
+//Incorrect PIN. Attempts left: 0
+//Maximum attempts reached. ATM card blocked.
 //Output//
-//Enter your 4-digit ATM PIN: 1234//
-//PIN correct. Access granted.//
+//Enter your 4-digit ATM PIN: 1234
+//PIN correct. Access granted.
 
