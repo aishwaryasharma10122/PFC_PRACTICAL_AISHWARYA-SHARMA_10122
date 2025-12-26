4dbe23791f4ebab0635fddb4a4d0b4b4389bf9fa
aishwaryasharma10122 <aishwaryasharma2711@gmail.com>
2025-12-26 19:23:50 +0530

    Update 8.2.c

--- Pratical 8/8.2.c
+++ Pratical 8/8.2.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // PRACTICAL 08:Q no.2:-Function to swap two numbers using pointers //
-void swap(int *a, int *b) {
+void swap(int *a, int *b) {  //Aishwarya Sharma, ERP-10122//
     int temp = *a;
     *a = *b;
     *b = temp;
@@ -17,4 +17,9 @@ int main() {
     printf("After swap: x = %d, y = %d\n", x, y);
 
     return 0;
-}
+} 
+//output//
+// Enter two numbers: 2
+// 6
+// Before swap: x = 2, y = 6
+// After swap: x = 6, y = 2
