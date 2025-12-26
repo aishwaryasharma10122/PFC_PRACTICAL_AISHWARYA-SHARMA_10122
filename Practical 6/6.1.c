489e2347cdc53ac680940257bb75b474e49dc78e
aishwaryasharma10122 <aishwaryasharma2711@gmail.com>
2025-12-26 18:48:41 +0530

    Update 6.1.c

--- Practical 6/6.1.c
+++ Practical 6/6.1.c
@@ -18,6 +18,6 @@ int main() {      //PRACTICAL 6 :-Question no.1:-Print numbers from a to b using
     return 0;
 }
 //Output//
-//Enter the starting number (a): 1//
-//Enter the ending number (b): 8//
-//Numbers from 1 to 8: 1 2 3 4 5 6 7 8 //
+//Enter the starting number (a): 1
+//Enter the ending number (b): 8
+//Numbers from 1 to 8: 1 2 3 4 5 6 7 8 
