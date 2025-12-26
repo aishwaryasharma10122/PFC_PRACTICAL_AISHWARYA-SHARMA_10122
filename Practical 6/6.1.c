17d04107da5e8b87fa266076da70c89881c35228
aishwaryasharma10122 <aishwaryasharma2711@gmail.com>
2025-12-26 12:22:23 +0530

    Update 6.1.c

--- Practical 6/6.1.c
+++ Practical 6/6.1.c
@@ -1,7 +1,7 @@
  #include <stdio.h>
 
 int main() {      //PRACTICAL 6 :-Question no.1:-Print numbers from a to b using loops//
-    int a, b;
+    int a, b; //Aishwarya Sharma, ERP-10122//
 
     printf("Enter the starting number (a): ");
     scanf("%d", &a);
@@ -17,3 +17,7 @@ int main() {      //PRACTICAL 6 :-Question no.1:-Print numbers from a to b using
 
     return 0;
 }
+//Output//
+//Enter the starting number (a): 1//
+//Enter the ending number (b): 8//
+//Numbers from 1 to 8: 1 2 3 4 5 6 7 8 //
